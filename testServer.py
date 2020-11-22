20c7bfa2b2c8b5c8e8c094fc499505b0ead9648e
sharkwhale0313 <71440792+sharkwhale0313@users.noreply.github.com>
2020-11-22 14:08:57 +0900

    commit

--- testServer.py
+++ testServer.py
@@ -13,12 +13,12 @@ def home():
     return render_template('testHtml.html')
 
 
-@app.route('/age')
+@app.route('/age',methods=)
 def age():
     return render_template('select-age.html')
 
 
-@app.route('/budget')
+@app.route('/budget?')
 def budget():
     return render_template('select-budget.html')
 
@@ -49,26 +49,26 @@ def result():
 
 
 ## API 역할을 하는 부분
-@app.route('/tag', methods=['POST'])
-def write_tag():
-    # 클라이언트가 준 tags 가져오기
-    tags_receive = request.form.getlist('tags_give[]')
-    title_receive = request.form['title']
-    price_receive = request.form['price']
-    url_receive = request.form['url']
-    db.testgift.insert_one({"title": title_receive, "price": price_receive, "url": url_receive, "tags": tags_receive})
-
-    # 성공 여부 & 성공 메시지 반환
-    return jsonify({'result': 'success', 'msg': '태그 저장 성공'})
-
-
-@app.route('/tag', methods=['GET'])
-def read_candidates():
-    # 1. DB에서 크롤링 정보 모두 가져오기
-    candidates = list(db.test.find({}, {'_id': 0}))
-    # 2. 성공 여부 & 리뷰 목록 반환하기
-    return jsonify({'result': 'success', 'candidates': candidates})
-
+# @app.route('/tag', methods=['POST'])
+# def write_tag():
+#     # 클라이언트가 준 tags 가져오기
+#     tags_receive = request.form.getlist('tags_give[]')
+#     title_receive = request.form['title']
+#     price_receive = request.form['price']
+#     url_receive = request.form['url']
+#     db.testgift.insert_one({"title": title_receive, "price": price_receive, "url": url_receive, "tags": tags_receive})
+#
+#     # 성공 여부 & 성공 메시지 반환
+#     return jsonify({'result': 'success', 'msg': '태그 저장 성공'})
+#
+#
+# @app.route('/tag', methods=['GET'])
+# def read_candidates():
+#     # 1. DB에서 크롤링 정보 모두 가져오기
+#     candidates = list(db.test.find({}, {'_id': 0}))
+#     # 2. 성공 여부 & 리뷰 목록 반환하기
+#     return jsonify({'result': 'success', 'candidates': candidates})
+#
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
