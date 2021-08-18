cd16a9d85e204e5dec5a8600ea3ac201a95470c7
sharkwhale0313 <71440792+sharkwhale0313@users.noreply.github.com>
2021-08-18 17:56:54 +0900

    Update testServer.py

--- testServer.py
+++ testServer.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('localhost', 27017)  # mongoDB 27017 포트
 db = client.giftpick
 
 
